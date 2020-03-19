a5b91cdfa0c5bb9e2ae01df743b5c20b8ccad961
Võ Quang Hòa <voquanghoa@gmail.com>
2020-03-19 14:37:16 +0700

    Update

--- src/test/java/MathClassTest.java
+++ src/test/java/MathClassTest.java
@@ -18,7 +18,7 @@ public class MathClassTest {
         assertEquals(mathClass.max(5, 4, 2), 5);
         assertEquals(mathClass.max(2, 4, 6), 6);
 
-        assertEquals(mathClass.max(5, 4, 3), 6);
+        assertEquals(mathClass.max(5, 4, 3), 5);
         assertEquals(mathClass.max(5, 4, 6), 6);
         assertEquals(mathClass.max(5, 5, 6), 6);
     }
